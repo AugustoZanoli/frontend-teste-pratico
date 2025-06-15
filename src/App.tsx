10730483201d64173ca31fe70fc16d3aa25bf60d
Zanoli <d2023001020@unifei.edu.br>
2025-06-15 20:55:08 -0300

    [FEAT] - Criação da home page e rotas

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,17 @@
-import './App.css'
-import Investimentos from './pages/Investimentos'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Investimentos from "./pages/Investimentos";
+import './App.css';
 
 function App() {
-
-  return (
-    <>
-      <Investimentos />
-    </>
-  )
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/investimentos" element={<Investimentos />} />
+            </Routes>
+        </BrowserRouter>
+    );
 }
 
-export default App
+export default App;
